71c7550ff6e0ab757d3596785e2bea18380dd760
Sunghwan <chak1212@naver.com>
2022-07-11 21:34:19 +0900

    인스타그램 봇 옵션 처리 및 테스트 추가

--- instagram_automation_bot/instagram_automation_bot.py
+++ instagram_automation_bot/instagram_automation_bot.py
@@ -1,7 +1,12 @@
 
+
 class InstagramAutomationBot:
 
     def __init__(self, webdriver):
+        self.user_id = None
+        self.user_password = None
+        self.crawing_target_id_list = None
+
         self.insta_url = 'https://www.instagram.com/'
         self.login_url = self.insta_url + 'accounts/login'
 
@@ -16,7 +21,31 @@ class InstagramAutomationBot:
         self.webdriver = webdriver
         return
 
-
     def setOption(self, option):
+        option_list = [
+            ("user_id",self.setUserID),
+            ("user_passwd", self.setUserPassword),
+            ("crawing_target_id_list", self.setCrawingTargetIdList)
+        ]
+        for key, set_func in option_list:
+            if key in option:
+                set_func(option[key])
         return
 
+    def setUserID(self, user_id):
+        self.user_id = user_id
+
+    def getUserID(self):
+        return self.user_id
+
+    def setUserPassword(self, user_password):
+        self.user_password = user_password
+
+    def getUserPassword(self):
+        return self.user_password
+
+    def setCrawingTargetIdList(self, crawing_target_id_list):
+        self.crawing_target_id_list = crawing_target_id_list
+
+    def getCrawingTargetList(self):
+        return self.crawing_target_id_list
--- instagram_automation_bot/instagram_automation_bot_test.py
+++ instagram_automation_bot/instagram_automation_bot_test.py
@@ -1,19 +1,19 @@
 import unittest
 
-from instagram_automation_bot.instagram_automation_bot import InstagramAutomationBot
+from instagram_automation_bot import InstagramAutomationBot
 
 
 class InstagramAutomationBotTest(unittest.TestCase):
 
-    def bot_option_test(self):
+    def test_bot_option(self):
         bot_option = {
             "user_id" : "aaaaaa",
             "user_passwd" : "bbbbbb",
-            "crawing_target_id" : ["aaaa1", "aaaa2", "aaaa3"]
+            "crawing_target_id_list" : ["aaaa1", "aaaa2", "aaaa3"]
         }
         insta_bot = InstagramAutomationBot(None)
         insta_bot.setOption(bot_option)
-        self.assertEqual(insta_bot.getUserID(), "aaaaa")
+        self.assertEqual(insta_bot.getUserID(), "aaaaaa")
         self.assertEqual(insta_bot.getUserPassword(), "bbbbbb")
         self.assertEqual(insta_bot.getCrawingTargetList(), ["aaaa1", "aaaa2", "aaaa3"])
 
