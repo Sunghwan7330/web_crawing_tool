06c6655a8578a53ad8d6b381dbc135db35291505
Sunghwan <chak1212@naver.com>
2022-07-12 20:46:06 +0900

    인스타그램 봇 로그 추가

--- instagram_automation_bot/instagram_automation_bot.py
+++ instagram_automation_bot/instagram_automation_bot.py
@@ -1,12 +1,16 @@
+import logging
 
 
 class InstagramAutomationBot:
 
     def __init__(self, webdriver):
+        self.logger = None
+        self.webdriver = None
         self.user_id = None
         self.user_password = None
         self.crawing_target_id_list = None
 
+        self.setDefaultLogger()
         self.insta_url = 'https://www.instagram.com/'
         self.login_url = self.insta_url + 'accounts/login'
 
@@ -17,13 +21,28 @@ class InstagramAutomationBot:
         self.setWebdriver(webdriver)
         return
 
+
+    def setDefaultLogger(self):
+        self.logger = logging.getLogger("InstagramAutomationBot")
+        self.setLogLevel(logging.INFO)
+        self.setLogFormat('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+
+    def setLogLevel(self, level):
+        self.logger.setLevel(level)
+
+    def setLogFormat(self, format):
+        formatter = logging.Formatter(format)
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        self.logger.addHandler(stream_handler)
+
     def setWebdriver(self, webdriver):
         self.webdriver = webdriver
         return
 
     def setOption(self, option):
         option_list = [
-            ("user_id",self.setUserID),
+            ("user_id", self.setUserID),
             ("user_passwd", self.setUserPassword),
             ("crawing_target_id_list", self.setCrawingTargetIdList)
         ]
