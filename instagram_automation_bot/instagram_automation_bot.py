91b9c043843eb526b51c5dc1f370e46911fa2b9d
Sunghwan <chak1212@naver.com>
2022-07-16 21:46:53 +0900

    인스타그램 사용자 정보 크롤링 추가
    * 난독화로 매번 태그가 달라져서 키워드를 검색하여 찾는 방법으로 변경

--- instagram_automation_bot/instagram_automation_bot.py
+++ instagram_automation_bot/instagram_automation_bot.py
@@ -1,5 +1,8 @@
 import logging
+import time
 
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
 class InstagramAutomationBot:
 
@@ -10,6 +13,7 @@ class InstagramAutomationBot:
         self.user_password = None
         self.crawing_target_id_list = None
 
+        self.setWebdriver(webdriver)
         self.setDefaultLogger()
         self.insta_url = 'https://www.instagram.com/'
         self.login_url = self.insta_url + 'accounts/login'
@@ -21,6 +25,33 @@ class InstagramAutomationBot:
         self.setWebdriver(webdriver)
         return
 
+    def run_automation(self):
+        if self.webdriver is None:
+            self.logger.error("Webdriver is not setting.")
+            return False
+
+        if self.user_id is None:
+            self.logger.error("user id is not setting.")
+            return False
+
+        if self.user_password is None:
+            self.logger.error("user password is not setting.")
+            return False
+
+        driver = self.webdriver
+        driver.get(self.login_url)
+        driver.implicitly_wait(3)
+
+        driver.find_element('xpath', self.xpath_id_field).send_keys(self.user_id)
+        driver.find_element('xpath', self.xpath_password_field).send_keys(self.user_password)
+        driver.find_element('xpath', self.xpath_login_btn).click()
+        driver.implicitly_wait(5)
+        time.sleep(5)
+
+        return True
+
+    def setWebdriver(self, webdriver):
+        self.webdriver = webdriver
 
     def setDefaultLogger(self):
         self.logger = logging.getLogger("InstagramAutomationBot")
@@ -50,6 +81,32 @@ class InstagramAutomationBot:
             if key in option:
                 set_func(option[key])
         return
+
+    def getKeywordFirstTagValue(self, html, keyword):
+        idx = html.find(keyword) + len(keyword)
+
+        temp = html[idx:idx + 100]
+        target_tag = ''
+        for i in range(len(temp)):
+            if temp[i] != '<':
+                continue
+            idx = temp.find(' ', i, len(temp))
+            target_tag = temp[i+1:idx]
+            break
+
+        soup = BeautifulSoup(temp, 'html.parser')
+        res = soup.select(target_tag)
+        if len(res) == 0:
+            return None
+
+        return res[0].text
+
+    def getUserInfoFromHtml(self, html):
+        feed_cnt = int(self.getKeywordFirstTagValue(html, '게시물'))
+        follower_cnt = int(self.getKeywordFirstTagValue(html, '팔로워'))
+        follow_cnt = int(self.getKeywordFirstTagValue(html, '팔로우'))
+
+        return [feed_cnt, follower_cnt, follow_cnt]
 
     def setUserID(self, user_id):
         self.user_id = user_id
